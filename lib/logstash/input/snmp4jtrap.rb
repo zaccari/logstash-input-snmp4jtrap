58ca0fd360a998b478def122027b6fb02d01e74e
Michael Zaccari <michael.zaccari@accelerated.com>
2016-05-02 22:12:03 -0400

    Add initial implementation for snmp4jrtrap

--- lib/logstash/input/snmp4jtrap.rb
+++ lib/logstash/input/snmp4jtrap.rb
@@ -1,9 +1,90 @@
-require "logstash/input/snmp4jtrap/version"
+# encoding: utf-8
+require 'logstash/inputs/base'
+require 'logstash/namespace'
 
-module Logstash
-  module Input
-    module Snmp4jtrap
-      # Your code goes here...
+# Read snmp trap messages as events with optional custom MIB support.
+
+class LogStash::Inputs::Snmp4JTrap < LogStash::Inputs::Base
+  config_name 'snmp4jtrap'
+
+  # The address to listen on
+  config :host, validate: :string, default: '0.0.0.0'
+
+  # The port to listen on. Remember that ports less than 1024 (privileged
+  # ports) may require root to use. hence the default of 10162.
+  config :port, validate: :number, default: 1162
+
+  # Transport protocol
+  config :protocol, validate: ['udp', 'tcp'], default: 'udp'
+
+  # Directory containing MIB files
+  config :mib_dir, validate: :string, default: '/usr/share/snmp/mibs'
+
+  # SNMP4J-SMI PRO License Key
+  config :license_key, validate: :string
+
+  def initialize(*args)
+    super(*args)
+  end # def initialize
+
+  def register
+    require 'snmp4jr'
+    @snmptrap = nil
+    load_mib_manager
+  end
+
+  def run(output_queue)
+    begin
+      snmptrap_listener(output_queue)
+    rescue => e
+      @logger.warn('SNMP4J Trap listener died', exception: e, backtrace: e.backtrace)
+      Stud.stoppable_sleep(5) { stop? }
+      retry if !stop?
     end
+  end
+
+  def stop
+    @snmptrap.close if @snmptrap
+    @snmptrap = nil
+  end
+
+  private
+
+  def load_mib_manager
+    @mib_manager = SNMP4JR::MibManager.instance
+
+    @mib_manager.license_key = @license_key
+    @mib_manager.mib_directory = @mib_dir
+
+    @mib_manager.load
+  end
+
+  def build_trap_listener
+    listener_opts = {
+      protocol: @protocol,
+      host: @host,
+      port: @port
+    }
+
+    @logger.info("It's a Trap!", listener_opts.dup)
+
+    @snmptrap = SNMP4JR::TrapListener.new(listener_opts)
+  end
+
+  def snmptrap_listener(output_queue)
+    build_trap_listener
+
+    @snmptrap.on_trap do |trap|
+      begin
+        event = LogStash::Event.new(trap.to_h)
+        decorate(event)
+        @logger.debug('SNMP Trap received: ', trap_object: trap.inspect)
+        output_queue << event
+      rescue => error
+        @logger.error('Failed to create event', trap_object: trap.inspect)
+      end
+    end
+
+    @snmptrap.join
   end
 end
--- spec/logstash/input/snmp4jtrap_spec.rb
+++ spec/logstash/input/snmp4jtrap_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Logstash::Input::Snmp4jtrap do
-  it 'has a version number' do
-    expect(Logstash::Input::Snmp4jtrap::VERSION).not_to be nil
-  end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
